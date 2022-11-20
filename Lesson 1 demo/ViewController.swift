da892e78d4daf1789dc5bf9fac8e5d06dce034b1
sergeiKalinin35 <piolersa85@mail.ru>
2022-11-20 13:28:03 +0200

    isHidden

--- Lesson 1 demo/ViewController.swift
+++ Lesson 1 demo/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 class ViewController: UIViewController {
     
     
+    @IBOutlet weak var helloWorldLabel: UILabel!
+    @IBOutlet var textButton: UIButton!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        helloWorldLabel.isHidden = true
+        textButton.layer.cornerRadius = 10
+        
+        
        
     }
+    
+    
+    @IBAction func textButtonPressed() {
+        
+        if helloWorldLabel.isHidden {
+            helloWorldLabel.isHidden = false
+            textButton.setTitle("Hide Text", for: .normal)
+            
+        } else {
+            
+            helloWorldLabel.isHidden = true
+            textButton.setTitle("Show Text", for: .normal)
+        }
+        
+    }
+    
+    
+    
 
 
 }
